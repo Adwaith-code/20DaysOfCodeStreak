5e6e2ea6a301ba0ccd43a161b81947e3fe126056
Adwaith-code <59331421+Adwaith-code@users.noreply.github.com>
2023-01-15 19:33:18 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,15 @@
+function stringup(x){
+    return(x.toUpperCase());
+}
+
+function stringlow(y){
+    return(y.toLowerCase());
+}
+
+function stringcount(z){
+    return(z.length);
+}
+
+console.log("Upper Case:"+stringup("hello world"));
+console.log("Lower Case:"+stringlow("HELLO WORLD"));
+console.log("Length:"+stringcount("Hello World"));
